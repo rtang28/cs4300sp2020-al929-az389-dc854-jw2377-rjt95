e64f93066687c06ce706645b463a7d353be8f5d9
Andy Zhu <azhu99@gmail.com>
2020-05-01 00:11:03 -0400

    clean up unnecessary files

---
+++ site/src/search/AdvancedSearch.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AdvancedSearch = () => {
+  return (
+    <div className='advanced-search'>
+
+    </div>
+  );
+};
+
+AdvancedSearch.propTypes = {
+
+};
+
+export default AdvancedSearch;
